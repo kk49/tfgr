4793074e47ab0485e324fdaaa1d23359074ee93e
Krys Kamieniecki <krys@kamieniecki.com>
2020-04-02 01:43:21 -0400

    Basic NPC/enemy record, looks like trash but should be enough for combat prototype

---
+++ campaign/scripts/npc_main.lua
@@ -0,0 +1,32 @@
+function onInit()
+	update();
+end
+
+function VisDataCleared()
+	update();
+end
+
+function InvisDataAdded()
+	update();
+end
+
+function updateControl(sControl, bReadOnly, bForceHide)
+	if not self[sControl] then
+		return false;
+	end
+		
+	return self[sControl].update(bReadOnly, bForceHide);
+end
+
+function update()
+	local nodeRecord = getDatabaseNode();
+	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
+
+	skill.setReadOnly(bReadOnly);
+	stamina.setReadOnly(bReadOnly);
+	initiative.setReadOnly(bReadOnly);
+	armour.setReadOnly(bReadOnly);
+	description.setReadOnly(bReadOnly);
+	special.setReadOnly(bReadOnly);
+	mien.setReadOnly(bReadOnly);
+end
